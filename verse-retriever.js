eea9a01ab7dfb67e38385f478a7e254aeeac8b71
hchiam <hchiam@users.noreply.github.com>
2020-11-18 01:57:37 -0500

    iife

--- verse-retriever.js
+++ verse-retriever.js
@@ -1,124 +1,134 @@
-let numberOfApiCalls = 0;
+// const data = await verseRetriever.getVerseWords("Genesis 1:1");
 
-let timer;
+const verseRetriever = (function () {
+  let numberOfApiCalls = 0;
 
-const bookNames = [
-  // Note: Put 1 john before john. For easier processing later: some unpluralized, some reordered, and some alternates.
-  // OT: 39
-  "Genesis",
-  "Exodus",
-  "Leviticus",
-  "Numbers",
-  "Deuteronomy",
-  "Joshua",
-  "Judges",
-  "Ruth",
-  "1st Samuel",
-  "1 Samuel",
-  "2nd Samuel",
-  "2 Samuel",
-  "1st Kings",
-  "1 Kings",
-  "2nd Kings",
-  "2 Kings",
-  "1st Chronicles",
-  "1 Chronicles",
-  "2nd Chronicles",
-  "2 Chronicles",
-  "Ezra",
-  "Nehemiah",
-  "Esther",
-  "Job",
-  "Psalms",
-  "Psalm",
-  "Proverbs",
-  "Proverb",
-  "Ecclesiastes",
-  "Song of Songs",
-  "Song of Solomon",
-  "Songs of Solomon",
-  "Isaiah",
-  "Jeremiah",
-  "Lamentations",
-  "Lamentation",
-  "Ezechiel",
-  "Daniel",
-  "Hosea",
-  "Joel",
-  "Amos",
-  "Obadiah",
-  "Jonah",
-  "Micah",
-  "Nahum",
-  "Habakkuk",
-  "Zephaniah",
-  "Haggai",
-  "Zechariah",
-  "Malachi",
-  // NT: 27
-  "Matthew",
-  "Mark",
-  "Luke",
-  "1st John",
-  "1 John",
-  "2nd John",
-  "2 John",
-  "3rd John",
-  "3 John",
-  "John",
-  "Acts",
-  "Romans",
-  "1st Corinthians",
-  "1 Corinthians",
-  "2nd Corinthians",
-  "2 Corinthians",
-  "Galatians",
-  "Ephesians",
-  "Philippians",
-  "Colossians",
-  "1st Thessalonians",
-  "1 Thessalonians",
-  "2nd Thessalonians",
-  "2 Thessalonians",
-  "1st Timothy",
-  "2nd Timothy",
-  "Titus",
-  "Philemon",
-  "Hebrews",
-  "James",
-  "1st Peter",
-  "1 Peter",
-  "2nd Peter",
-  "2 Peter",
-  "Jude",
-  "Revelation",
-  "Revelations",
-];
+  let timer;
 
-async function getVerseWords(searchText) {
-  if (!searchText) return;
+  const bookNames = [
+    // Note: Put 1 john before john. For easier processing later: some unpluralized, some reordered, and some alternates.
+    // OT: 39
+    "Genesis",
+    "Exodus",
+    "Leviticus",
+    "Numbers",
+    "Deuteronomy",
+    "Joshua",
+    "Judges",
+    "Ruth",
+    "1st Samuel",
+    "1 Samuel",
+    "2nd Samuel",
+    "2 Samuel",
+    "1st Kings",
+    "1 Kings",
+    "2nd Kings",
+    "2 Kings",
+    "1st Chronicles",
+    "1 Chronicles",
+    "2nd Chronicles",
+    "2 Chronicles",
+    "Ezra",
+    "Nehemiah",
+    "Esther",
+    "Job",
+    "Psalms",
+    "Psalm",
+    "Proverbs",
+    "Proverb",
+    "Ecclesiastes",
+    "Song of Songs",
+    "Song of Solomon",
+    "Songs of Solomon",
+    "Isaiah",
+    "Jeremiah",
+    "Lamentations",
+    "Lamentation",
+    "Ezechiel",
+    "Daniel",
+    "Hosea",
+    "Joel",
+    "Amos",
+    "Obadiah",
+    "Jonah",
+    "Micah",
+    "Nahum",
+    "Habakkuk",
+    "Zephaniah",
+    "Haggai",
+    "Zechariah",
+    "Malachi",
+    // NT: 27
+    "Matthew",
+    "Mark",
+    "Luke",
+    "1st John",
+    "1 John",
+    "2nd John",
+    "2 John",
+    "3rd John",
+    "3 John",
+    "John",
+    "Acts",
+    "Romans",
+    "1st Corinthians",
+    "1 Corinthians",
+    "2nd Corinthians",
+    "2 Corinthians",
+    "Galatians",
+    "Ephesians",
+    "Philippians",
+    "Colossians",
+    "1st Thessalonians",
+    "1 Thessalonians",
+    "2nd Thessalonians",
+    "2 Thessalonians",
+    "1st Timothy",
+    "2nd Timothy",
+    "Titus",
+    "Philemon",
+    "Hebrews",
+    "James",
+    "1st Peter",
+    "1 Peter",
+    "2nd Peter",
+    "2 Peter",
+    "Jude",
+    "Revelation",
+    "Revelations",
+  ];
 
-  if (!containsBookName(searchText)) return;
+  async function getVerseWords(searchText) {
+    if (!searchText) return;
 
-  numberOfApiCalls++;
-  if (numberOfApiCalls > 100) {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      numberOfApiCalls = 0;
-    }, 6 * 60 * 60 * 1000);
-    return;
+    if (!containsBookName(searchText)) return;
+
+    numberOfApiCalls++;
+    if (numberOfApiCalls > 100) {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        numberOfApiCalls = 0;
+      }, 6 * 60 * 60 * 1000);
+      return;
+    }
+
+    const urlAPICall = `https://bibleverse.glitch.me/get-verse/${searchText}`;
+    return await fetch(urlAPICall)
+      .then((res) => res.json())
+      .then((res) => {
+        const verseWordsHtml = res.content;
+        const copyright = res.copyright.trim();
+        return { words: verseWordsHtml, copyright };
+      })
+      .catch((error) => console.log(error));
   }
 
-  const urlAPICall = `https://bibleverse.glitch.me/get-verse/${searchText}`;
-  await fetch(urlAPICall)
-    .then((res) => res.json())
-    .then((res) => {
-      const copyright = res.copyright;
-      const content = res.content;
-      return res;
-    });
-}
+  function containsBookName(text) {
+    return bookNames.find((book) => text.includes(book));
+  }
 
-function containsBookName(text) {
-  return bookNames.find((book) => text.includes(book));
-}
+  return {
+    getVerseWords,
+    containsBookName,
+  };
+})();
